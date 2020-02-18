cd1c04c908fd038f0dd20ed3358ebc09b703e213
Nikolay Chebotaryov <rododin@gmail.com>
2020-02-18 20:16:39 +0300

    Issue 7: Web Service - introduced:  - The `ws` module has been started on the top of configuration generated at https://start.spring.io/.  - Some minor manual fixes have been done.

---
+++ modules/ws/src/main/java/org/rododin/algorithmsws/AlgorithmsWSApplication.java
@@ -0,0 +1,13 @@
+package org.rododin.algorithmsws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgorithmsWSApplication
+{
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AlgorithmsWSApplication.class, args);
+	}
+}
---
+++ modules/ws/src/test/java/org/rododin/algorithmsws/AlgorithmsWSApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rododin.algorithmsws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlgorithmsWSApplicationTests
+{
+	@Test
+	void contextLoads()
+	{
+	}
+}
