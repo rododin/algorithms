035a6790cfc1e1f44f51b8a916a8f34d9bae02e7
Nikolay Chebotaryov <rododin@gmail.com>
2020-02-22 23:24:47 +0300

    HackerRank - Encryption - solved:  - A trivial/simple solution has been implemented.  - Appropriate unit tests added.

--- modules/hackerrank/src/main/java/org/rododin/algorithms/hackerrank/HackerRank.java
+++ modules/hackerrank/src/main/java/org/rododin/algorithms/hackerrank/HackerRank.java
@@ -1,8 +1,7 @@
 package org.rododin.algorithms.hackerrank;
 
 import org.rododin.algorithms.Constants;
-import org.rododin.algorithms.hackerrank.easy.RepeatedString;
-import org.rododin.algorithms.hackerrank.medium.ExtraLongFactorials;
+import org.rododin.algorithms.hackerrank.medium.Encryption;
 
 /**
  * Just an app stub for quick testing.
@@ -14,24 +13,11 @@ public class HackerRank
 {
 	public static void main(String[] args)
 	{
-		ExtraLongFactorials.printExtraLongFactorials( 1);
-		ExtraLongFactorials.printExtraLongFactorials( 2);
-		ExtraLongFactorials.printExtraLongFactorials( 3);
-		ExtraLongFactorials.printExtraLongFactorials( 4);
-		ExtraLongFactorials.printExtraLongFactorials( 5);
-		ExtraLongFactorials.printExtraLongFactorials( 6);
-		ExtraLongFactorials.printExtraLongFactorials( 7);
-		ExtraLongFactorials.printExtraLongFactorials( 8);
-		ExtraLongFactorials.printExtraLongFactorials( 9);
-		ExtraLongFactorials.printExtraLongFactorials(10);
-		ExtraLongFactorials.printExtraLongFactorials(20);
-		ExtraLongFactorials.printExtraLongFactorials(30);
-		ExtraLongFactorials.printExtraLongFactorials(40);
-		ExtraLongFactorials.printExtraLongFactorials(50);
-		ExtraLongFactorials.printExtraLongFactorials(60);
-		ExtraLongFactorials.printExtraLongFactorials(70);
-		ExtraLongFactorials.printExtraLongFactorials(80);
-		ExtraLongFactorials.printExtraLongFactorials(90);
-		ExtraLongFactorials.printExtraLongFactorials(100);
+		Encryption.printEncryption("");
+		Encryption.printEncryption("      ");
+		Encryption.printEncryption("have a nice day");
+		Encryption.printEncryption("feed the dog    ");
+		Encryption.printEncryption("  chill out  ");
+		Encryption.printEncryption("if man was meant to stay on the ground god would have given us roots");
 	}
 }
