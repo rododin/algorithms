0942f32be05fd40c2508ac363fd609bb742a2afc
Nikolay Chebotaryov <rododin@gmail.com>
2022-07-05 18:11:10 +0300

    HackerRank > Practice > Algorithms > Cut the sticks - solved:  - Another more effective solution has been implemented.    The complexity is `O(n + O(standardJavaIntegerListSortAlgorithm))`.    It doesn't introduce any additional constraints compared with the previously implemented recursive `cutTheSticksRecursive(List, List)` algorithm.  - Unit tests have been added. Now the new algorithm well and quickly passes the test with 1 million elements.

--- modules/hackerrank/src/main/java/org/rododin/algorithms/hackerrank/problem_solving/easy/CutTheSticks.java
+++ modules/hackerrank/src/main/java/org/rododin/algorithms/hackerrank/problem_solving/easy/CutTheSticks.java
@@ -4,6 +4,7 @@
 
 package org.rododin.algorithms.hackerrank.problem_solving.easy;
 
+import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -19,12 +20,11 @@ public class CutTheSticks
 	/**
 	 * Implements a solution for the <a href="https://www.hackerrank.com/challenges/cut-the-sticks/problem">Cut the sticks</a> problem.
 	 * The method signature is precisely the same as provided in the HackerRank code template.
-	 * Internally it passes the job to the other recursive {@link #cutTheSticks(List, List)} method.
 	 * <p/>
 	 * NOTE:
-	 *   The complexity is {@code O(2n(n-1))}. Thus, the solution is not effective.
-	 *   Also, the recursion introduces additional constraints disallowing to handle too huge input arrays.
-	 *   However it still passed all the HackerRank tests and was accepted.
+	 *   The complexity is {@code O(n + O(standardJavaIntegerListSortAlgorithm))}. This solution is pretty effective.
+	 *   It doesn't introduce any additional constraints compared with the recursive {@link #cutTheSticksRecursive(List, List)} algorithm.
+	 *   It passed well all the HackerRank tests and was accepted.
 	 *
 	 * @param arr non-<code>null</code> list of the stick lengths to be cut.
 	 * @return empty list if the given {@code arr} is empty,
@@ -33,15 +33,42 @@ public class CutTheSticks
 	 */
 	public static List<Integer> cutTheSticks(List<Integer> arr)
 	{
-		final List<Integer> rv = new LinkedList<>();
-		cutTheSticks(arr, rv);
-		return rv;
+		final List<Integer> result = new LinkedList<>();
+
+		if (arr.size() == 0)
+			return result;
+		result.add(arr.size());
+
+		Collections.sort(arr);
+
+		int curLength = arr.get(0);
+		for (int i = 0; i < arr.size(); i++)
+		{
+			if (arr.get(i) > curLength)
+			{
+				curLength = arr.get(i);
+				result.add(arr.size() - i);
+			}
+		}
+		return result;
 	}
 
-	private static void cutTheSticks(List<Integer> arr, List<Integer> result)
+	/**
+	 * Implements a solution for the <a href="https://www.hackerrank.com/challenges/cut-the-sticks/problem">Cut the sticks</a>
+	 * problem in recursive way. The complexity is {@code O(2n(n-1))}. Thus, the solution is not effective.
+	 * Also, the recursion introduces additional constraints disallowing to handle too huge input arrays.
+	 * However it still passed all the HackerRank tests and was accepted.
+	 *
+	 * @param arr non-<code>null</code> list of the stick lengths to be cut.
+	 * @param result the list to store the produced results. It remains empty if the given {@code arr} is empty,
+	 *               or it becomes a singleton list of {@code 1} if the given {@code arr} size is 1,
+	 *               or it becomes a list of numbers of sticks processed on each iteration according to the task definition
+	 * @throws StackOverflowError on too huge {@code arr} given (e.g. 1 million of elements)
+	 */	private static void cutTheSticksRecursive(List<Integer> arr, List<Integer> result)
 	{
-		if (arr.size() > 0)
-			result.add(arr.size());
+		if (arr.size() == 0)
+			return;
+		result.add(arr.size());
 
 		final List<Integer> nextIteration = new LinkedList<>();
 		int min = Integer.MAX_VALUE;
@@ -56,6 +83,6 @@ public class CutTheSticks
 				nextIteration.add(e - min);
 		}
 		if (nextIteration.size() > 0)
-			cutTheSticks(nextIteration, result);
+			cutTheSticksRecursive(nextIteration, result);
 	}
 }
