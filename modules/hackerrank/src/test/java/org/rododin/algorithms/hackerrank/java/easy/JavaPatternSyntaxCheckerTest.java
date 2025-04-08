62fce7db7668353228a1286f9cb4cd232a9f4f22
Nikolai Chebotarev <rododin@chess.com>
2025-04-08 19:25:50 +0300

    Issue #59: HackerRank > Prepare > Java > Strings > Pattern Syntax Checker - completed:  - The problem solution has been introduced within the class `JavaPatternSyntaxChecker`.  - A unit test has been added within `JavaPatternSyntaxCheckerTest`.
    Issue #57: HackerRank > Prepare > Java > Introduction > Java String Tokens - Some refactoring done:
     - The `doTest()` and `stringArrayToStringByLine()` have been moved to the newly introduced `AbstractTest` class for further reusing within `JavaPatternSyntaxCheckerTest` and other tests in the future.

---
+++ modules/hackerrank/src/test/java/org/rododin/algorithms/hackerrank/java/easy/JavaPatternSyntaxCheckerTest.java
@@ -0,0 +1,22 @@
+package org.rododin.algorithms.hackerrank.java.easy;
+
+import org.junit.Test;
+import org.rododin.algorithms.hackerrank.AbstractTest;
+
+/**
+ * Unit Test(s) for <code>{@link JavaPatternSyntaxChecker}</code>.
+ *
+ * @author Nikolay Chebotaryov (Rod Odin)
+ */
+public class JavaPatternSyntaxCheckerTest
+	extends AbstractTest
+{
+	@Test
+	public void testSimple()
+	{
+		doTest( stringArrayToStringByLine("3", "([A-Z])(.+)", "[AZ[a-z](a-z)", "batcatpat(nat")
+		      , () -> JavaPatternSyntaxChecker.main(null)
+		      , stringArrayToStringByLine("Valid", "Invalid", "Invalid")
+		);
+	}
+}
--- modules/hackerrank/src/test/java/org/rododin/algorithms/hackerrank/java/easy/JavaStringTokensTest.java
+++ modules/hackerrank/src/test/java/org/rododin/algorithms/hackerrank/java/easy/JavaStringTokensTest.java
@@ -1,14 +1,7 @@
 package org.rododin.algorithms.hackerrank.java.easy;
 
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
-import java.io.PrintStream;
-import java.util.Arrays;
-import java.util.stream.Collectors;
-
-import org.junit.Assert;
 import org.junit.Test;
+import org.rododin.algorithms.hackerrank.AbstractTest;
 
 /**
  * Unit Test(s) for <code>{@link JavaStringTokens}</code>.
@@ -16,12 +9,14 @@ import org.junit.Test;
  * @author Nikolay Chebotaryov (Rod Odin)
  */
 public class JavaStringTokensTest
+	extends AbstractTest
 {
 	@Test
 	public void simplestTaskTest()
 	{
 		doTest( "He is a very very good boy, isn't he?"
-		      , stringArrayToStringByLine(new String[] {"10", "He", "is", "a", "very", "very", "good", "boy", "isn", "t", "he"})
+		      , () -> JavaStringTokens.main(null)
+		      , stringArrayToStringByLine("10", "He", "is", "a", "very", "very", "good", "boy", "isn", "t", "he")
 		      );
 	}
 
@@ -29,28 +24,8 @@ public class JavaStringTokensTest
 	public void trailingSpacesTest()
 	{
 		doTest( "           YES      leading spaces        are valid,    problemsetters are         evillllll"
-		      , stringArrayToStringByLine(new String[] {"8", "YES", "leading", "spaces", "are", "valid", "problemsetters", "are", "evillllll"})
+		      , () -> JavaStringTokens.main(null)
+		      , stringArrayToStringByLine("8", "YES", "leading", "spaces", "are", "valid", "problemsetters", "are", "evillllll")
 		      );
-	}
-
-	private void doTest(String testString, String expectedResult)
-	{
-		final InputStream systemStdIn = System.in;
-		final PrintStream systemStdOut = System.out;
-		System.setIn(new ByteArrayInputStream(testString.getBytes()));
-		final ByteArrayOutputStream output = new ByteArrayOutputStream(64 * 1024);
-		System.setOut(new PrintStream(output));
-		JavaStringTokens.main(null);
-		final String producedResult = output.toString();
-		System.setOut(systemStdOut);
-		System.setIn(systemStdIn);
-		System.out.printf(producedResult);
-
-		Assert.assertEquals(expectedResult.trim(), producedResult.trim());
-	}
-
-	private static String stringArrayToStringByLine(String[] strings)
-	{
-		return Arrays.stream(strings).collect(Collectors.joining(System.lineSeparator()));
 	}
 }
