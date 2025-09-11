ae5068473565e8f1154c137720fdf8a27384be29
Nikolai Chebotarev <rododin@chess.com>
2025-09-11 23:39:18 +0300

    Issue #94: LeetCode > Problems > Add Two Numbers - solved:  - 3 solutions have been implemented:     * `addTwoNumbersVersion1Slow(ListNode l1, ListNode l2)` - it's based on conversion of the numbers to `String`s, then to `BigInteger`s, then performing the addition operation for 2 `BigInteger`s using the `BigInteger.add()` API, then conversion of the result back to `String` and `LinkedNode` chain appropriately. Thus, it works pretty slowly.     * `addTwoNumbersVersion2Fast(ListNode l1, ListNode l2)` - it's a direct implementation of the mathematical decimal place-wise addition algorithm (well known as "column addition"). It works about 8 times faster compared with the `*Version1*`.     * `addTwoNumbersVersion3MostOptimal(ListNode l1, ListNode l2)` - even more optimized version of the columns addition algorithm excluding some duplicated checks in the loop.  - Appropriate unit tests have been added, including misc edge cases.  - NOTE: The algorithm works well with `null` operands and generally is unlimited by the length of the numbers, i.e. should work well with operands consisting of thousands and even millions of decimal places, however we tested only 200.

---
+++ modules/leetcode/src/main/java/org/rododin/algorithms/leetcode/Misc.java
@@ -0,0 +1,13 @@
+package org.rododin.algorithms.leetcode;
+
+/**
+ * Just the module main class stub, useful to have for quick experiments.
+ *
+ * @author Nikolay Chebotaryov
+ */
+public class Misc
+{
+	public static void main(String[] args)
+	{
+	}
+}
